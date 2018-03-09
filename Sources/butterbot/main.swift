ad627f81b9442abeca904c8dd9814b74300c0f13
Allan Vialatte <allan.vialatte@icloud.com>
2018-03-09 15:48:40 +0100

    let's go

--- Sources/butterbot/main.swift
+++ Sources/butterbot/main.swift
@@ -9,18 +9,14 @@ bot.addRTMBotWithAPIToken(slackBotToken)
 
 bot.notificationForEvent(.hello) { (event, message) in
   if let channelID = event.channel?.id {
-    try? bot.rtm?.sendMessage("Hello les gros fils de pute", channelID: channelID)
+    try? bot.rtm?.sendMessage("I am alive", channelID: channelID)
   }
-}
-
-bot.notificationForEvent("app_mention") { (event, client) in
-  print(event.message)
 }
 
 bot.notificationForEvent(.message) { (event, _) in
   print(event.message)
   if let channelID = event.channel?.id {
-    try? bot.rtm?.sendMessage("Tu parles trop gros connard", channelID: channelID)
+    try? bot.rtm?.sendMessage("I am alive", channelID: channelID)
   }
 }
 
