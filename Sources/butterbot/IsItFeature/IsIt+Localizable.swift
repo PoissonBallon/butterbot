08f39b7cb6cb82d433b946012a4b283e8db44cfd
Nicolas SZYMENDERA <szymenderanicolas@gmail.com>
2018-03-12 18:19:53 +0100

    Add Localizable strings on IsIT and Karma features

--- Sources/butterbot/IsItFeature/IsIt+Localizable.swift
+++ Sources/butterbot/IsItFeature/IsIt+Localizable.swift
@@ -8,27 +8,48 @@
 import Foundation
 
 struct IIL10n {
-  static let yes_1 = "Yes :thumbsup:"
-  static let yes_2 = "Owyyyyy :thumbsup:"
-  static let yes_3 = "Je veux mon vieux :thumbsup:"
-  static let yes_4 = "Plutôt deux fois qu'une !! :thumbsup:"
-  static let yes_5 = "Evidemment :thumbsup:"
+  static let yes_01 = "Yes :thumbsup:"
+  static let yes_02 = "Owyyyyy :thumbsup:"
+  static let yes_03 = "Je veux mon vieux :thumbsup:"
+  static let yes_04 = "Plutôt deux fois qu'une !! :thumbsup:"
+  static let yes_05 = "Evidemment :thumbsup:"
+  static let yes_06 = "Sure :thumbsup:"
+  static let yes_07 = "Oui :thumbsup:"
+  static let yes_08 = "Bien sure :thumbsup:"
+  static let yes_09 = "Of course :thumbsup:"
+  static let yes_10 = "Obviously :thumbsup:"
+
   
-  static let yes = [IIL10n.yes_1,
-                    IIL10n.yes_2,
-                    IIL10n.yes_3,
-                    IIL10n.yes_4,
-                    IIL10n.yes_5]
+  static let yes = [IIL10n.yes_01,
+                    IIL10n.yes_02,
+                    IIL10n.yes_03,
+                    IIL10n.yes_04,
+                    IIL10n.yes_05,
+                    IIL10n.yes_06,
+                    IIL10n.yes_07,
+                    IIL10n.yes_08,
+                    IIL10n.yes_09,
+                    IIL10n.yes_10]
   
-  static let not_1 = "Nop :thumbsdown:"
-  static let not_2 = "Je ne pense pas :thumbsdown:"
-  static let not_3 = "Never never :thumbsdown:"
-  static let not_4 = "Non ça m'étonnerait :thumbsdown:"
-  static let not_5 = "Non mais je veux du beurre :thumbsdown:"
+  static let not_01 = "Nop :thumbsdown:"
+  static let not_02 = "Je ne pense pas :thumbsdown:"
+  static let not_03 = "Never never :thumbsdown:"
+  static let not_04 = "Non ça m'étonnerait :thumbsdown:"
+  static let not_05 = "Non mais je veux du beurre :thumbsdown:"
+  static let not_06 = "Heu........ Nope :thumbsdown:"
+  static let not_07 = "I don't think so :thumbsdown:"
+  static let not_08 = "Même pas dans tes rêves les plus fous :thumbsdown:"
+  static let not_09 = "Non :thumbsdown:"
+  static let not_10 = "Nope Nope Nope Nope :thumbsdown:"
   
-  static let not = [IIL10n.not_1,
-                    IIL10n.not_2,
-                    IIL10n.not_3,
-                    IIL10n.not_4,
-                    IIL10n.not_5]
+  static let not = [IIL10n.not_01,
+                    IIL10n.not_02,
+                    IIL10n.not_03,
+                    IIL10n.not_04,
+                    IIL10n.not_05,
+                    IIL10n.not_06,
+                    IIL10n.not_07,
+                    IIL10n.not_08,
+                    IIL10n.not_09,
+                    IIL10n.not_10]
 }
